be97ec4eb0066dca35913e40488b87ac7f4f1516
Max Erastov <max.erastov@gmail.com>
2023-06-07 21:29:02 +0300

    Removed repetitions until the password length exceeds the number of available characters

--- src/generator.js
+++ src/generator.js
@@ -1,41 +1,56 @@
 const digits = '0123456789';
 const lowercase = 'abcdefghijklmnopqrstuvwxyz';
 const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const symbols = ' !@#$%^&*_-+=`|\\(){}[]:;"\'<>,.?/';
+const symbols = '!@#$%^&*_-+=`|\\(){}[]:;"\'<>,.?/';
+
 const getRandomInt = (min, max) => Math.floor(min + Math.random() * (max - min + 1));
-const calcComplexity = (pwdLen, charSetLen) => Math.round(Math.log2(charSetLen ** pwdLen), 2);
+
+const calcComplexity = (pwdLen, charsLen) => Math.round(Math.log2(charsLen ** pwdLen), 2);
+
+const deleteCharFromChars = (items, index) => {
+  const item = items[index];
+  const tempSet = new Set(...[items.split('')]);
+  tempSet.delete(item);
+  return [...tempSet].join('');
+};
 
 const generator = (params) => {
   let password = '';
-  let charSet = '';
+  let chars = '';
   const length = params.pwdLength;
   if (params.useDigits) {
-    charSet += digits;
+    chars += digits;
   }
   if (params.useLowerCase) {
-    charSet += lowercase;
+    chars += lowercase;
   }
   if (params.useUpperCase) {
-    charSet += uppercase;
+    chars += uppercase;
   }
   if (params.useSymbols) {
-    charSet += symbols;
+    chars += symbols;
   }
+  let tempChars = chars;
   for (let i = 0; i < length; i += 1) {
-    password += charSet.charAt(getRandomInt(0, charSet.length - 1));
+    if (tempChars.length === 0) {
+      tempChars = chars;
+    }
+    const randomIndex = getRandomInt(0, tempChars.length - 1);
+    password += tempChars.charAt(randomIndex);
+    tempChars = deleteCharFromChars(tempChars, randomIndex);
   }
-  const complexity = calcComplexity(password.length, charSet.length);
+  const complexity = calcComplexity(password.length, chars.length);
   return { password, complexity };
 };
 
 const multyGenerator = (params, num) => {
-  for (let i = 0; i <= num; i += 1) {
+  for (let i = 1; i <= num; i += 1) {
     console.log(generator(params));
   }
 };
 
 const testParams = {
-  pwdLength: 20, useDigits: true, useLowerCase: true, useUpperCase: true, useSymbols: true,
+  pwdLength: 10, useDigits: true, useLowerCase: false, useUpperCase: false, useSymbols: false,
 };
 
 multyGenerator(testParams, 10);
