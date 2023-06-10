b3bfd7429c8b84040ef3736568f0cca0cb03264c
koshun-code <sit.com.k@gmail.com>
2023-06-10 14:25:50 +0300

    added route to fromn page

--- src/Routes/route.js
+++ src/Routes/route.js
@@ -1,9 +1,12 @@
+import loginController from '../Controllers/loginController.js';
+
 export default (app) => {
-  app.get('/login', (req, res) => {
-    res.send('<h1>Login</h1>');
-  });
+  app.get('/login', loginController);
 
   app.get('/signup', (req, res) => {
     res.send('this is send message register page');
+  });
+  app.get('/', (req, res) => {
+    res.send('i\'m main page');
   });
 };
