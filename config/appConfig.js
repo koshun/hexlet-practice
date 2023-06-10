42153bcde7029726d5ed08cd95d9fe737ae37f0c
koshun-code <sit.com.k@gmail.com>
2023-06-10 22:03:45 +0300

    add config folder and config files for app and db

---
+++ config/appConfig.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import handlebars from 'express-handlebars';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+export default () => {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const app = express();
+  app.engine('hbs', handlebars.engine({
+    layoutsDir: join(__dirname, '/../src/Views/layouts'),
+    defaultLayout: 'layout',
+    extname: 'hbs',
+    partialsDir: join(__dirname, '/../src/Views/partials/'),
+  }));
+  app.set('views', './src/Views'); // './src/Views'
+  app.set('view engine', 'hbs');
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.static(join(__dirname, '/../public')));
+  return app;
+};
