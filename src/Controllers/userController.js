760920925033fffb4ba942fdd035085bb20416c6
koshun-code <sit.com.k@gmail.com>
2023-06-11 21:43:04 +0300

    changed UserConfroller to use user.model

--- src/Controllers/userController.js
+++ src/Controllers/userController.js
@@ -1,3 +1,19 @@
+import sequelize from '../../config/dbConfig.js';
+import User from '../Models/user.model.js';
+
 export default (req, res) => {
-  res.render('success', { pageTitle: 'Main page', title: 'This is main page' });
+  sequelize.sync().then(() => {
+    User.create({
+      login: 'superstar',
+      email: 'email@mail.com',
+      password: 'password',
+    }).then((result) => {
+      res.render('success', { pageTitle: 'Main page', title: `This is main page ${result.id}` });
+      console.log(result.id);
+    }).catch((error) => {
+      console.error('Failed to create a new recor:', error);
+    });
+  }).catch((e) => {
+    console.error('unable to create table', e);
+  });
 };
