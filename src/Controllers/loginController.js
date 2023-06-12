8f71e819e864926655de589bfa1a16710e351380
koshun-code <sit.com.k@gmail.com>
2023-06-12 18:16:06 +0300

    changed loginController. Makde it as an object

--- src/Controllers/loginController.js
+++ src/Controllers/loginController.js
@@ -1,3 +1,4 @@
-export default (reg, res) => {
-  res.render('login', { pageTitle: 'Login', title: 'This is a login page' });
+export default {
+  index: (req, res) => res.render('login', { pageTitle: 'Login page', title: 'This is login page' }),
+  store: (req, res) => res.redirect('/'),
 };
