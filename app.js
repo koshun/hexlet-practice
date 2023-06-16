d83c02eb542e18aa566365c7af64e91aed9e015b
koshun-code <sit.com.k@gmail.com>
2023-06-16 12:58:43 +0300

    move sequelize sync to app file

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 import route from './src/Routes/route.js';
 import config from './config/appConfig.js';
+import sequelize from './config/dbConfig.js';
 
 const port = process.env.PORT || 3001;
 
 const app = config();
 
 route(app);
-
-app.listen(port, () => console.log(`server work on port ${port}`));
+sequelize.sync().then(() => {
+  app.listen(port, () => console.log(`server work on port ${port}`));
+}).catch((e) => {
+  console.error('unable to create table', e);
+});
